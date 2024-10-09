45e85135c606709df2203c9b9a77b08228cf882a
CGDR <christiangerarddelosreyes@gmail.com>
2024-10-09 17:18:31 +0800

    feat: added clear log command

---
+++ app/Console/Commands/ClearLogs.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ClearLogs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clear-logs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear all log files in the logs directory';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $logsDir = storage_path('logs');
+
+        if (is_dir($logsDir)) {
+            $files = scandir($logsDir);
+            foreach ($files as $file) {
+                if (pathinfo($file, PATHINFO_EXTENSION) === 'log') {
+                    file_put_contents("$logsDir/$file", ""); // Clear file content
+                }
+            }
+            $this->info('All log files have been cleared.');
+        } else {
+            $this->error("Directory $logsDir does not exist.");
+        }
+    }
+}
