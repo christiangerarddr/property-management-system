c138154a3278537a9f92f8d864112074ffb7e5fd
CGDR <christiangerarddelosreyes@gmail.com>
2024-10-11 10:38:57 +0800

    refactor: moved store request validations to StorePropertyRequest

--- app/Http/Controllers/Properties/PropertyController.php
+++ app/Http/Controllers/Properties/PropertyController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Properties;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePropertyRequest;
 use App\Http\Responses\ResponseInterface;
 use App\Services\Contracts\PropertyServiceInterface;
 use Exception;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
@@ -24,44 +24,9 @@ class PropertyController extends Controller
         $this->propertyService = $propertyService;
     }
 
-    private function validator(Request $request)
+    public function store(StorePropertyRequest $request)
     {
-        return $request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'price' => 'required|numeric',
-            'size' => 'required|numeric',
-            'bedrooms' => 'numeric|nullable',
-            'bathrooms' => 'numeric|nullable',
-            'parking_spaces' => 'numeric|nullable',
-            'lease_terms' => 'numeric|nullable',
-            'date_built' => 'date|nullable',
-            'property_type' => 'numeric|nullable',
-            'status' => 'numeric|nullable',
-            'condition' => 'numeric|nullable',
-            'location.block_number' => 'required|numeric',
-            'location.lot_number' => 'required|numeric',
-            'location.street' => 'required|string|max:255',
-            'location.village' => 'required|string|max:255',
-            'location.city' => 'required|string|max:255',
-            'location.region' => 'required|string|max:255',
-            'features' => 'nullable|array',
-            'features.*.feature' => 'required|string|max:255',
-            'features.*.description' => 'nullable|string',
-            'owner.name' => 'nullable|string|max:255',
-            'owner.contact_info' => 'nullable|string|max:255',
-            'property_images' => 'nullable|array',
-            'property_images.*.image_name' => 'required|string|max:255',
-            'property_images.*.image_path' => 'required|string|max:255',
-            'amenities' => 'nullable|array',
-            'amenities.*.name' => 'required|string|max:255',
-            'amenities.*.description' => 'required|string|max:255',
-        ]);
-    }
-
-    public function store(Request $request)
-    {
-        $data = $this->validator($request);
+        $data = $request->validated();
         $property = [];
 
         DB::beginTransaction();
---
+++ app/Http/Requests/StorePropertyRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePropertyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+            'size' => 'required|numeric',
+            'bedrooms' => 'nullable|numeric',
+            'bathrooms' => 'nullable|numeric',
+            'parking_spaces' => 'nullable|numeric',
+            'lease_terms' => 'nullable|numeric',
+            'date_built' => 'nullable|date',
+            'property_type' => 'nullable|numeric',
+            'status' => 'nullable|numeric',
+            'condition' => 'nullable|numeric',
+            'location.block_number' => 'required|numeric',
+            'location.lot_number' => 'required|numeric',
+            'location.street' => 'required|string|max:255',
+            'location.village' => 'required|string|max:255',
+            'location.city' => 'required|string|max:255',
+            'location.region' => 'required|string|max:255',
+            'features' => 'nullable|array',
+            'features.*.feature' => 'required|string|max:255',
+            'features.*.description' => 'nullable|string',
+            'owner.name' => 'nullable|string|max:255',
+            'owner.contact_info' => 'nullable|string|max:255',
+            'property_images' => 'nullable|array',
+            'property_images.*.image_name' => 'required|string|max:255',
+            'property_images.*.image_path' => 'required|string|max:255',
+            'amenities' => 'nullable|array',
+            'amenities.*.name' => 'required|string|max:255',
+            'amenities.*.description' => 'required|string|max:255',
+        ];
+    }
+}
